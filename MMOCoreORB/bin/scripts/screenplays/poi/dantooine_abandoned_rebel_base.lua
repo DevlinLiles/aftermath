e923ded500f22a25b7cd5b929d50a8dea539d737
c0pp3r <dominguez.joey@gmail.com>
2017-09-15 10:51:43 -0400

    first commit

---
+++ MMOCoreORB/bin/scripts/screenplays/poi/dantooine_abandoned_rebel_base.lua
@@ -0,0 +1,43 @@
+AbandonedRebelBaseScreenPlay = ScreenPlay:new {
+	numberOfActs = 1,
+
+	screenplayName = "AbandonedRebelBaseScreenPlay",
+
+}
+
+registerScreenPlay("AbandonedRebelBaseScreenPlay", true)
+
+function AbandonedRebelBaseScreenPlay:start()
+	if (isZoneEnabled("dantooine")) then
+		self:spawnMobiles()
+	end
+end
+
+function AbandonedRebelBaseScreenPlay:spawnMobiles()
+	--inside perimeter near east building area
+	local pNpc = spawnMobile("dantooine", "rebel_commando", 400, -6656.4, 30.0, 5552.4, -1, 0)
+
+	if (pNpc ~= nil) then
+		HelperFuncs:setMobileTemplate(pNpc, "idlewander")
+	end
+
+	pNpc = spawnMobile("dantooine", "rebel_commando", 400, -6661.4, 30.0, 5557.4, -1, 0)
+
+	if (pNpc ~= nil) then
+		HelperFuncs:setMobileTemplate(pNpc, "idlewander")
+	end
+
+	pNpc = spawnMobile("dantooine", "rebel_commando", 400, -6651.4, 30.0, 5547.4, -1, 0)
+
+	if (pNpc ~= nil) then
+		HelperFuncs:setMobileTemplate(pNpc, "idlewander")
+	end
+
+	pNpc = spawnMobile("dantooine", "rebel_commando", 400, -6663.4, 30.0, 5562.4, -1, 0)
+
+	if (pNpc ~= nil) then
+		HelperFuncs:setMobileTemplate(pNpc, "idlewander")
+	end
+
+	spawnMobile("dantooine", "rebel_recruiter", 0,-6817,46,5511,174,0)
+end
